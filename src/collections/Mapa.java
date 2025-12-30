6371954756dca04c7e1de53e3858cff35eb90663
Rafael Brito <rafaelbritovaladares@gmail.com>
2025-12-30 13:18:48 -0300

    Estudando e implementando Map
    Muito interessante quando você precisar fazer uma lista com relação de chave e valor

---
+++ src/collections/Mapa.java
@@ -0,0 +1,44 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Mapa {
+
+	public static void main(String[] args) {
+		Map<Integer, String> usuarios = new HashMap<>();
+
+		usuarios.put(1, "Roberto");
+		usuarios.put(20, "Ricardo");
+		usuarios.put(3, "Rafaela");
+		usuarios.put(4, "Rebeca");
+		
+		System.out.println(usuarios.size());	
+		System.out.println(usuarios.isEmpty());
+		System.out.println(usuarios.keySet());
+		System.out.println(usuarios.values());
+		System.out.println(usuarios.entrySet());
+		System.out.println(usuarios.containsKey(20));
+		System.out.println(usuarios.containsValue("Rebeca"));
+		
+		System.out.println(usuarios.get(20));
+		System.out.println(usuarios.remove(1));
+		System.out.println(usuarios.remove(4, "Gui"));
+		
+		for(int x: usuarios.keySet()) {
+			System.out.println(x);
+		}
+		
+		for(String x: usuarios.values()) {
+			System.out.println(x);
+		}
+		
+		for(Entry<Integer, String> x: usuarios.entrySet()) {
+			System.out.print(x.getKey() + "===> ");
+			System.out.println(x.getValue());
+			System.out.println(x);
+		}
+	}
+
+}
