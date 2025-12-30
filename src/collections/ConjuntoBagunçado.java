f6f416211bf70d4891482a8f2f163dd54f0b2ff0
Rafael Brito <rafaelbritovaladares@gmail.com>
2025-12-30 07:46:53 -0300

    Finalizando a segunda parte do set

--- src/collections/ConjuntoBagunçado.java
+++ src/collections/ConjuntoBagunçado.java
@@ -1,8 +1,10 @@
 package collections;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class ConjuntoBagunçado {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		
 		HashSet conjunto = new HashSet();
@@ -29,6 +31,22 @@ public class ConjuntoBagunçado {
 		System.out.println(conjunto.contains('x'));
 		System.out.println(conjunto.contains(1));
 		System.out.println(conjunto.contains(true));
+		
+		Set nums = new HashSet();
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		
+		System.out.println(nums);
+		System.out.println(conjunto);
+		
+		// conjunto.addAll(nums); // União entre dois conjuntos
+		conjunto.retainAll(nums); // Conceito de interseção o valor comum entre os dois conjuntos
+		System.out.println(conjunto);
+		
+		conjunto.clear();
+		System.out.println(conjunto);
+		
 		
 	}
 }
